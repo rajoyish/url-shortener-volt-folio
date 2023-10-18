a78efd49ec2c5aa56915be7c1b80a54c74b27e0e
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-18 21:57:28 +0545

    Storing a shortened URL

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,34 +1,56 @@
- <?php
- use function Livewire\Volt\{state, rules, form};
- use App\Livewire\Forms\UrlForm;
- 
- form(UrlForm::class);
- 
- $submit = function () {
-     $this->form->validate();
- };
- ?>
+<?php
+use function Livewire\Volt\{state, rules, form};
+use App\Livewire\Forms\UrlForm;
+use App\Models\Url;
 
- <x-app-layout>
-     @volt
-         <form wire:submit="submit" wire:model="form.url">
-             <input type="text" id="url"
-                 class="w-full rounded-lg border-slate-300 text-slate-800 
-                 h-14 px-5 text-lg placeholder:text-slate-400 focus:ring-2 
-                 focus:ring-blue-500"
-                 placeholder="e.g. https://google.com">
+state('url');
 
-             <div class="flex items-baseline space-x-4">
-                 <button type="submit"
-                     class="bg-blue-500 text-blue-50 rounded-lg
+form(UrlForm::class);
+
+$submit = function () {
+    $this->form->validate();
+
+    $this->url = Url::create(
+        $this->form->only('url') + [
+            'hashid' => 'abc',
+        ],
+    );
+};
+?>
+
+<x-app-layout>
+    @volt
+        <form wire:submit="submit">
+            @if ($url)
+                <div>
+                    <p>Boom &mdash; your short link is ready!</p>
+                    <div class="mt-4">
+                        <input type="text" id="url"
+                            class="w-full rounded-lg border-slate-300 text-slate-800
+                         h-14 px-5 text-lg placeholder:text-slate-400 focus:ring-2
+                         focus:ring-blue-500"
+                            value="{{ $url->id }}" readonly>
+                    </div>
+                </div>
+            @else
+                <input wire:model="form.url" type="text" id="url"
+                    class="w-full rounded-lg border-slate-300 text-slate-800 
+                     h-14 px-5 text-lg placeholder:text-slate-400 focus:ring-2 
+                     focus:ring-blue-500"
+                    placeholder="e.g. https://google.com">
+            @endif
+
+            <div class="flex items-baseline space-x-4">
+                <button type="submit"
+                    class="bg-blue-500 text-blue-50 rounded-lg
                  px-6 h-10 font-medium inset-y-2 right-2 mt-2">
-                     Get Short URL
-                 </button>
+                    Get Short URL
+                </button>
 
-                 @error('form.url') 
-                     <div>{{ $message }}</div>
-                 @enderror
-             </div>
-         </form>
-     @endvolt
- </x-app-layout>
+                @error('form.url')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+        </form>
+    @endvolt
+</x-app-layout>
