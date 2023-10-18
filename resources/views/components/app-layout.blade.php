2f38c0e17707ac42edd238c42e00c937f1f31e98
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-18 08:42:13 +0545

    Submitting the form

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,13 +1,18 @@
  <!DOCTYPE html>
  <html lang="en">
+
  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="https://laravel.com/img/logomark.min.svg" type="image/x-icon">
-    <title>{{ config('app.name') }}</title> 
-    @vite(['resources/css/app.css', 'resource s/app/app.js'])
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="shortcut icon" href="https://laravel.com/img/logomark.min.svg" type="image/x-icon">
+     <title>{{ config('app.name') }}</title>
+     @vite(['resources/css/app.css', 'resource s/app/app.js'])
  </head>
+
  <body>
-    {{ $slot }}
+     <div class="max-w-2xl mt-32 mx-auto">
+         {{ $slot }}
+     </div>
  </body>
+
  </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,17 +1,34 @@
  <?php
- use function Livewire\Volt\{state};
- state(['counter' => 0]);
- $increment = function () {
-    $this->counter++;
- }
+ use function Livewire\Volt\{state, rules};
+ state('url');
+ 
+ rules(['url' => 'required|url']);
+ 
+ $submit = function () {
+     $this->validate();
+ };
  ?>
 
  <x-app-layout>
      @volt
-     <div> 
-        {{ $counter }}
- 
-        <button wire:click="increment">Increment</button>
-    </div> 
+         <form wire:submit="submit" wire:model="url">
+             <input type="text" id="url"
+                 class="w-full rounded-lg border-slate-300 text-slate-800 
+                 h-14 px-5 text-lg placeholder:text-slate-400 focus:ring-2 
+                 focus:ring-blue-500"
+                 placeholder="e.g. https://google.com">
+
+             <div class="flex items-baseline space-x-4">
+                 <button type="submit"
+                     class="bg-blue-500 text-blue-50 rounded-lg
+                 px-6 h-10 font-medium inset-y-2 right-2 mt-2">
+                     Get Short URL
+                 </button>
+
+                 @error('url')
+                     <div>{{ $message }}</div>
+                 @enderror
+             </div>
+         </form>
      @endvolt
  </x-app-layout>
